8cdffa8f64ba77c327ff8c1d9da48d17c9a25523
Tawan-ok <saiordday1@gmail.com>
2025-05-24 14:06:50 +0700

    first commit

---
+++ src/test/java/bookingManagementSystem/example/bookingManagementSystem/BookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bookingManagementSystem.example.bookingManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
